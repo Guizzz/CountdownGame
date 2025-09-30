98c99eb04b0e5142823a80f903889999c1f488be
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2025-09-30 23:41:25 +0200

    fixed leaderboard

--- components/LeaderBoard.tsx
+++ components/LeaderBoard.tsx
@@ -88,8 +88,8 @@ export default function LeaderBoard(){
 
     return (
         <div >
-            <div className={isVisible?"absolute w-full h-full bg-black/50 z-10":""} onClick={()=>{ isVisible?close():"" }}/>
-            <div className=" absolute right-5 bottom-5 flex flex-row items-center ">
+            <div className={isVisible?" fixed w-full h-full bg-black/50 z-10":""} onClick={()=>{ isVisible?close():"" }}/>
+            <div className=" fixed right-5 bottom-5 flex flex-row items-center ">
                 <div className={showInfo?"flex flex-row items-center":"hidden"}>
                     <div className=" text-xl min-md:text-2xl min-lg:text-4xl font-story_script">
                         Try the LeaderBoard!
@@ -115,7 +115,7 @@ export default function LeaderBoard(){
                         animate={{ opacity: 1, scale: 1 }}
                         exit={{ opacity: 0, scale: 0 }}
                         key="box"
-                        className="z-20 m-10 min-md:m-auto p-2 left-0 right-0 top-1/6 absolute min-md:w-2/3 min-lg:w-1/2 rounded-md backdrop-blur-sm bg-white/30 shadow-xl"
+                        className="z-20 m-10 min-md:m-auto p-2 left-0 right-0 top-1/6 fixed min-md:w-2/3 min-lg:w-1/2 rounded-md backdrop-blur-sm bg-white/30 shadow-xl"
                     >
                         <div >
                             <div className=" flex flex-col items-center justify-center m-4">
